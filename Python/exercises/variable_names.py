66917ef13e31395082cd999ef79afe436b06bd7c
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-27 16:13:24 +0000

    Refactoring/debugging exercise

---
+++ Python/exercises/variable_names.py
@@ -0,0 +1,27 @@
+country = 'United Kingdom'
+
+fruits = ['apple', 'orange', 'banana']
+
+my_set = {'James', 23, 'Software Developer'}
+
+multiplier = 4
+
+number_list = [2, 3, 4, 5, 6] 
+
+firstname_and_lastname = ('Alan', 'Turing')
+
+number = 22000
+
+x = 6 
+
+favourite_sport = 'Cricket' 
+
+I_am_the_best = True 
+
+_1st_place = 'Carol'
+
+_2nd_place = 'Dave'
+
+_3rd_place = 'Susan'
+
+last_place = 'Peter' 
